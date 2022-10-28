dbda0127db899afbffb7d89631fd02ebfdde268f
kallasz22 <99244927+kallasz22@users.noreply.github.com>
2022-10-28 21:50:45 +0200

    I don't think $PORT in server.js will work

--- server.js
+++ server.js
@@ -6,12 +6,12 @@ const app = express();
 const mongoose = require('mongoose');
 if (process.env.dbUsername) {
     mongoose.connect(`mongodb+srv://${process.env.dbUsername}:${process.env.dbPassword}@shelfiecluster.cow3v5m.mongodb.net/?retryWrites=true&w=majority`).then(function(){
-        app.listen(5000);
+        app.listen($PORT);
     });
 } else {
     const config = require('./config.json');
     mongoose.connect(`mongodb+srv://${config.dbUsername}:${config.dbPassword}@shelfiecluster.cow3v5m.mongodb.net/?retryWrites=true&w=majority`).then(function(){
-        app.listen(5000);
+        app.listen($PORT);
     });
 }
 
